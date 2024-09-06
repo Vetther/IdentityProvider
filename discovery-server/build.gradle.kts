67369d462f5fcafd21587a439e57c439508691dd
Vetther <osk4r.wolny@gmail.com>
2024-09-06 02:00:02 +0200

    Removed api-gateway

--- discovery-server/build.gradle.kts
+++ discovery-server/build.gradle.kts
@@ -1,3 +1,14 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
+
+tasks.withType<BootBuildImage> {
+	imageName = "vetther/${rootProject.name}-${project.name}"
+
+	environment.put("BP_JVM_CDS_ENABLED", "false")
+	environment.put("BP_SPRING_AOT_ENABLED", "false")
+
+	buildpacks = listOf("gcr.io/paketo-buildpacks/eclipse-openj9:latest", "paketo-buildpacks/java")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "kierowca2.0"
 
 include("identity-provider")
-include("api-gateway")
 include("discovery-server")
 include("mail-service")
